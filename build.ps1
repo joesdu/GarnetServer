1cb0147ffdf9a82a98a1843dc3c8a13aa1651cbd
Joes <dygood@outlook.com>
2024-05-05 15:22:15 +0800

    fix: 更新测试.

--- build.ps1
+++ build.ps1
@@ -1,15 +1,11 @@
 $gh = "ghcr.io/joesdu/";
 $name="garnet" ;
 
-# 构建有版本号的
+
 $tag=":latest" ;
 $imagename =$base + $name + $tag ;
 $imagename ;
-docker buildx create --use ;
-docker buildx build --platform linux/amd64,linux/arm64 -f ./GarnetServer/Dockerfile -t $imagename . --push --progress plain ;
 
-# 构建 latest
-#$tag=":latest" ;
-#$imagename =$base + $name + $tag ;
-#$imagename ;
-#docker build -f ./Dockerfile -t $imagename . ;
+docker buildx create --use ;
+
+docker buildx build --platform linux/amd64,linux/arm64 -f ./GarnetServer/Dockerfile -t $imagename . --progress plain --push ;
